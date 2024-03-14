405419d9d78b7fb3e9d10d19d5f64c94aaa6d49a
Ariful Islam <arifulislam758143@gmail.com>
2024-03-15 01:16:43 +0600

    Update setup_user.php

--- setup_user.php
+++ setup_user.php
@@ -10,9 +10,11 @@ include_once './db/connection.php';
 if ($data !== null) {
     // Process the received JSON data
     $id = isset($data['id']) ? intval($data['id']) : 0;
-    $name = mysqli_real_escape_string($con, $data['name']);
-    $user_name = mysqli_real_escape_string($con, $data['user_name']);
-    $pass = password_hash($data['pass'], PASSWORD_DEFAULT); // Use bcrypt or Argon2 for stronger hashing
+    $f_name = mysqli_real_escape_string($con, $data['firstName']);
+    $l_name = mysqli_real_escape_string($con, $data['lastName']);
+    $name = $f_name . " " . $l_name;
+    $user_name = mysqli_real_escape_string($con, $data['username']);
+    $pass = password_hash($data['password'], PASSWORD_DEFAULT); // Use bcrypt or Argon2 for stronger hashing
     $email = mysqli_real_escape_string($con, $data['email']);
     $created_at = date("Y-m-d"); // Use a format compatible with your database
 

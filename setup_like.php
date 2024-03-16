f6353e06d204af579bdfa7f6b12fed24a67b7230
Ariful Islam <arifulislam758143@gmail.com>
2024-03-16 22:59:43 +0600

    Update setup_like.php

--- setup_like.php
+++ setup_like.php
@@ -17,26 +17,37 @@ if ($data !== null) {
     $animal_id = mysqli_real_escape_string($con, $data['post']);
     $status = isset($data['status']) ? intval($data['status']) : 1;
 
-    if ($id > 0) {
-        $sql = "UPDATE `likes` SET `user_id`='$user_id', `animal_id`='$animal_id', `status`='$status' WHERE id=$id";
-        $message = "Updated Successfully";
-    } else {
-        $sql = "INSERT INTO `likes` (`user_id`, `animal_id`, `status`) VALUES ('$user_id', '$animal_id', '$status')";
-        $message = "Added Successfully";
-    }
 
-    if (mysqli_query($con, $sql)) {
+    $sql = "SELECT * FROM `likes` WHERE likes.user_id='$user_id'  AND likes.animal_id='$animal_id'";
+    $rslt = mysqli_query($con, $sql);
+
+    if (mysqli_num_rows($rslt) > 0) {
+        $message = "You Likes this Animal Before";
         $response = array(
             'error' => false,
             'message' => $message
         );
     } else {
-        $response = array(
-            'error' => true,
-            'message' => 'Error: ' . mysqli_error($con)
-        );
-    }
+        if ($id > 0) {
+            $sql = "UPDATE `likes` SET `user_id`='$user_id', `animal_id`='$animal_id', `status`='$status' WHERE id=$id";
+            $message = "Likes Updated Successfully";
+        } else {
+            $sql = "INSERT INTO `likes` (`user_id`, `animal_id`, `status`) VALUES ('$user_id', '$animal_id', '$status')";
+            $message = "Likes Added Successfully";
+        }
 
+        if (mysqli_query($con, $sql)) {
+            $response = array(
+                'error' => false,
+                'message' => $message
+            );
+        } else {
+            $response = array(
+                'error' => true,
+                'message' => 'Error: ' . mysqli_error($con)
+            );
+        }
+    }
     mysqli_close($con);
 } else {
     // Error in decoding JSON data
